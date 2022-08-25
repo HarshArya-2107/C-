9dca892ba26b2b412e078bee1b124f2d609c623c
Harsh Arya <72383821+HarshArya-2107@users.noreply.github.com>
2022-08-25 20:57:52 +0530

    C++

---
+++ Operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+ 
+    // pre increment
+    int a=10;
+    int b=++a;
+    
+    cout<<a<<endl;
+    cout<<b<<endl;
+   
+   // Post increment
+     int c=10;
+     int d=c++;
+     cout<<c<<" "<<d<<endl;
+}
